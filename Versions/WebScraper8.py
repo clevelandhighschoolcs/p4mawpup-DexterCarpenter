f6654e262a21dd60bc463b7479bdf609d3d89caa
Hunter Key <hunterjkey@gmail.com>
2017-12-11 17:22:21 -0800

    Update WebScraper8.py
    I added a working version of Twillo to the program (I worked on my computer but please try it o your own before replacing your code with this). I also updated the version number.

--- Versions/WebScraper8.py
+++ Versions/WebScraper8.py
@@ -1,6 +1,6 @@
 #
 # Web Scraper
-# Version: 7
+# Version: 8
 
 """
 cd C:\Users\Dexter Carpenter\Documents\GitHub\WebScraper\environment
@@ -19,13 +19,18 @@ cd C:\Users\dexte\Documents\GitHub\WebScraper\environment
 import urllib2
 from bs4 import BeautifulSoup
 import time
+from twilio.rest import Client
 
 #variables
 global tagcountnow
 global tagcountold
 global scrape_interval
-
-
+global Twillo_null #a variable that will determine the functionality of Twillo in this program
+Twillo_null = False
+global account_sid 
+global auth_token
+global twilio_phone_number
+global my_phone_number
 
 print ''
 print "Press 'Ctrl + C' to exit the Scraper"
@@ -48,6 +53,37 @@ print 'How often do you want to scrape the webpage? (seconds)'
 scrape_interval = raw_input()
 
 print ''
+#These next 20 lines have to do with Twillo and allowing that to work
+print ' '
+print "Would you like me to send you a text message when I find a change(y/n)? (You will need to gave a twillo number for this to work.)"
+txtmessage = raw_input()
+if txtmessage == "y":
+	print ' '
+	print 'Enter your account SID (all of the following can be obtained on your Twillo dashboard)'
+	account_sid = raw_input() 
+	
+	print ' '
+	print 'Enter your authentification token'
+	auth_token = raw_input()
+
+	print ' '
+	print 'Enter your Twillo phone number'
+	twilio_phone_number = raw_input()
+	if (twilio_phone_number[:2] != "+1"): #this ensures a "+1" is given at the beginning of the phone number
+		twilio_phone_number = "+1" + twilio_phone_number
+	
+	print ' '
+	print "Enter your own phone number"
+	my_phone_number = raw_input()
+	if (my_phone_number[:2] != "+1"): #this ensures a "+1" is given at the beginning of the phone number
+		my_phone_number = "+1" + my_phone_number
+	
+	#This makes sure that the info inputted is valid, if not, it skips this part.
+	if (len(account_sid) != 34) or (len(auth_token) != 32) or (len(twilio_phone_number) != 12) or (len(my_phone_number) != 12) or (twilio_phone_number[:2] != "+1") or (my_phone_number[:2] != "+1"):
+		Twillo_null = True #if this is triggered, the program will refrain from doing anything with the information given above
+		
+else:
+	Twillo_null = True
 
 print 'The Scraper will check the number of tags in the webpage.'
 print 'The Scraper will display "Change!" if the number of tags has changed from the previous scan.'
@@ -76,7 +112,12 @@ if __name__ == "__main__":
 def scraper():
 	global tagcountnow
 	global tagcountold
-	
+	global Twillo_null
+	global account_sid 
+	global auth_token
+	global twilio_phone_number
+	global my_phone_number
+
     # query the website and return the html to the variable page
 	page = urllib2.urlopen(quote_page)
     
@@ -88,10 +129,21 @@ def scraper():
 	
 	if tagcountnow == tagcountold:
 		print 'No Change'
+		body = 'No Change'
 	elif tagcountnow != tagcountold:
 		print 'Change!'
-	
+		body = 'Change!'
 	tagcountold = tagcountnow
+	
+	#This sends the message to your phone
+	if Twillo_null == False:
+		client = Client(account_sid, auth_token)
+		client.messages.create(
+			body=body,
+			to=my_phone_number,
+			from_=twilio_phone_number
+		)
+	
 
 while True:
 	
@@ -99,36 +151,3 @@ while True:
         scraper()
     
     time.sleep(float(scrape_interval))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
